4edf56bbaae30b6fccfcd7ed0cbf4446317c2c9f
sarah-linder <sarah@familie-linder.de>
2026-01-27 20:36:47 +0100

    Update touren.js

--- touren.js
+++ touren.js
@@ -1,6 +1,6 @@
 const touren = {
-  "45min-innenstadt-kehlturm": ["Kehlturm", "Tourist-Info", "Obertor"],
-  "45min-innenstadt-tourist-info": ["Tourist-Info", "Kehlturm"],
+  "45min-innenstadt-kehlturm": ["Kehlturm", "Romaneum", "Loewendenkmal", "Zeughaus", "Jakobusstatue", "Quirinus", "Vogthaus", "Marktbrunnen", "Rathaus", "Tourist-Info"],
+  "45min-innenstadt-tourist-info": ["Tourist-Info", "Marktbrunnen", "Rathaus", "Vogthaus", "Quirinus", "Jakobusstatue", "Zeughaus", "Loewendenkmal", "Romaneum", "Kehlturm"],
   
   "90min-innenstadt-kehlturm": ["Kehlturm", "Obertor", "Tourist-Info"],
   "90min-innenstadt-obertor": ["Obertor", "Tourist-Info"],
