214b3c30f2e1d416b32785c2eb2e7e4ab8968cd9
sarah-linder <sarah@familie-linder.de>
2026-01-27 20:28:49 +0100

    Create touren.js

---
+++ touren.js
@@ -0,0 +1,10 @@
+const touren = {
+  "45min-innenstadt-kehlturm": ["Kehlturm", "Tourist-Info", "Obertor"],
+  "45min-innenstadt-tourist-info": ["Tourist-Info", "Kehlturm"],
+  
+  "90min-innenstadt-kehlturm": ["Kehlturm", "Obertor", "Tourist-Info"],
+  "90min-innenstadt-obertor": ["Obertor", "Tourist-Info"],
+  
+  "45min-LAGA": ["LAGA-Start45", "LAGA-Station2", "LAGA-Station3"],
+  "90min-LAGA": ["LAGA-Start90", "LAGA-Station2", "LAGA-Station3"]
+};
